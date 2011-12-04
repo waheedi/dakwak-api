0561b9397ad740d9e9c1520b066e5622d3ad630f
Waheed Barghouthi <waheed@dakwak.com>
2011-12-04 07:11:23 -0500

    changed env

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 function New(name){
 	if(mongoose == null){
 		var mongoose = require('mongoose');
-		var db = mongoose.connect('mongodb://localhost/dakwak_development');
+		var db = mongoose.connect('mongodb://localhost/dakwak_production');
 		var Schema = mongoose.Schema;
 	}
 	var s = new Schema(); 
